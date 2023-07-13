4a34b689226e81373dff5d61f5ca29e9c3d156b8
Michal Makuch <michalmakuch@hotmail.com>
2023-07-13 14:28:36 +0200

    Test commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+// Just checking something...
 export default defineConfig({
+  base: "/lights-generator-app",
   plugins: [react()],
-})
+});
